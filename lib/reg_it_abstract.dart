ef291229d0cd1ecc3cdd2e4a3d22319119d36b34
dronn1k <100835727+dronn1kDev@users.noreply.github.com>
2025-01-24 15:40:54 +0200

    Model improvements (#1)
    * Major changes.
    
    Factory registrars was refactored.
    WeakSingletonRegistrar was implemented.
    
    * Weak Registrar was improved.
    
    * dependencies changes.
    
    flutter has been removed from dependencies.
    package version has been updated.
    
    * test fixes.
    
    flutter libraries has been deleted from test files.
    
    * MR fixes & Factory changes.

--- lib/reg_it_abstract.dart
+++ lib/reg_it_abstract.dart
@@ -1,89 +1,100 @@
 library reg_it_abstract;
 
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+
 abstract interface class Registrar<T> {
   T get instance;
 
   void dispose();
 }
 
-abstract class SyncFactory<T> implements Registrar<SyncFactory<T>> {
-  T call();
+mixin DisposeHandler<T> implements Registrar<T> {
+  @protected
+  abstract final void Function(Registrar<T> registrar)? beforeDispose;
+
+  @override
+  @mustCallSuper
+  void dispose() => beforeDispose?.call(this);
 }
 
-abstract class AsyncFactory<T> implements Registrar<AsyncFactory<T>> {
-  Future<T> call();
-}
-
-class SingletonRegistrar<T> implements Registrar<T> {
+class SingletonRegistrar<T> with DisposeHandler<T> implements Registrar<T> {
   @override
   final T instance;
 
-  const SingletonRegistrar(this.instance);
-
   @override
-  void dispose() {}
+  final void Function(Registrar<T> registrar)? beforeDispose;
+
+  const SingletonRegistrar(this.instance, {this.beforeDispose});
 }
 
-class SyncFactoryRegistrar<T> implements SyncFactory<T> {
+class InstanceFactoryRegistrar<T>
+    with DisposeHandler<T>
+    implements Registrar<T> {
   final T Function() _constructor;
 
-  const SyncFactoryRegistrar(this._constructor);
+  InstanceFactoryRegistrar(this._constructor, {this.beforeDispose});
 
   @override
-  SyncFactory<T> get instance => this;
+  T get instance => _constructor();
 
   @override
-  T call() => _constructor();
-
-  @override
-  void dispose() {}
+  final void Function(Registrar<T> registrar)? beforeDispose;
 }
 
-class AsyncFactoryRegistrar<T> implements AsyncFactory<T> {
-  final Future<T> Function() _constructor;
-
-  AsyncFactoryRegistrar(this._constructor);
-
-  @override
-  Future<T> call() => _constructor();
-
-  @override
-  AsyncFactory<T> get instance => this;
-
-  @override
-  void dispose() {}
+class AsyncFactoryRegistrar<T> extends InstanceFactoryRegistrar<FutureOr<T>> {
+  AsyncFactoryRegistrar(super.constructor, {super.beforeDispose});
 }
 
-class LazySingletonRegistrar<T> implements Registrar<T> {
+class LazySingletonRegistrar<T> with DisposeHandler<T> implements Registrar<T> {
   T? _instance;
 
   final T Function() _constructor;
 
-  LazySingletonRegistrar(this._constructor);
+  LazySingletonRegistrar(this._constructor, {this.beforeDispose});
 
   @override
   T get instance => _instance ??= _constructor();
 
   @override
-  void dispose() {}
+  final void Function(Registrar<T> registrar)? beforeDispose;
+}
+
+class WeakSingletonRegistrar<T extends Object>
+    with DisposeHandler<T>
+    implements Registrar<T> {
+  @override
+  final void Function(Registrar<T> registrar)? beforeDispose;
+
+  final T Function() _instanceBuilder;
+
+  WeakSingletonRegistrar(this._instanceBuilder, {this.beforeDispose});
+
+  WeakReference<T>? _weakReference;
+
+  @override
+  T get instance {
+    final weakInstance = _weakReference?.target;
+    if (weakInstance != null) return weakInstance;
+
+    final instance = _instanceBuilder();
+
+    if (instance is bool || instance is num || instance is String) {
+      return instance;
+    }
+
+    _weakReference = WeakReference(instance);
+
+    return instance;
+
+    // mb it's also works
+    // return (_weakReference = WeakReference(instance)).target!;
+  }
 }
 
 abstract interface class Registry {
   void put<T>(final Registrar<T> registrar);
   T get<T>();
   void drop<T>();
-}
-
-abstract class SmartFactory<T, A> implements Registrar<SmartFactory<T, A>> {
-  abstract final T Function(A args) builder;
-
-  T call(A args);
-}
-
-class SmartFactoryRegistrar<T, A> {
-  final T Function(A args) builder;
-
-  SmartFactoryRegistrar(this.builder);
-
-  T call(A args) => builder(args);
 }
--- test/abstract_di_container_test.dart
+++ test/abstract_di_container_test.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
 import 'dart:developer';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:meta/meta.dart';
 import 'package:reg_it_abstract/reg_it_abstract.dart';
+import 'package:test/test.dart';
 
 class TestRegistry implements Registry {
   final Map<Type, Registrar> _instanceMap = {};
@@ -18,6 +19,16 @@ class TestRegistry implements Registry {
 
   @override
   T get<T>() {
+    try {
+      final Registrar(:instance) = _instanceMap[T]!;
+      return instance;
+    } catch (e, st) {
+      log('get error', error: e, stackTrace: st);
+      return asyncGet();
+    }
+  }
+
+  T asyncGet<T extends Future>() {
     try {
       final Registrar(:instance) = _instanceMap[T]!;
       return instance;
@@ -58,20 +69,12 @@ class SomeService {
   SomeService(this.temp);
 }
 
-class ServiceFactory extends SmartFactoryRegistrar<SomeService, int>
-    implements Registrar<ServiceFactory> {
-  ServiceFactory(super.builder);
-
-  @override
-  void dispose() {}
-
-  @override
-  ServiceFactory get instance => this;
-}
-
 void main() {
   group('DI put & get testing', () {
     final TestRegistry di = TestRegistry.instance;
+    setUp(() {
+      di.instanceMap.clear();
+    });
     test('DI put singleton test', () {
       di.put(SingletonRegistrar(SomeService(0)));
       final mapLength = di.instanceMap.length;
@@ -79,127 +82,58 @@ void main() {
     });
 
     test('di double put exception test', () {
-      try {
-        di.put(SingletonRegistrar(SomeService(3)));
+      di.put(SingletonRegistrar(SomeService(3)));
+      expect(di.instanceMap.length, 1);
 
-        expect(di.instanceMap.length, 1);
-      } catch (e) {
-        expect(di.instanceMap.length, 1);
-      }
+      expect(() => di.put(SingletonRegistrar(SomeService(0))),
+          throwsA(isA<Exception>()));
+
+      expect(di.instanceMap.length, 1);
+      expect(di.get<SomeService>().temp, 3);
     });
 
     test('di get simple singleton testing', () {
+      di.put(SingletonRegistrar(SomeService(3)));
+      expect(di.instanceMap.length, 1);
+
       final intService = di.get<SomeService>();
 
-      expect(intService.temp, 0);
+      expect(intService.temp, 3);
+      expect(di.instanceMap.length, 1);
     });
 
-    test('some singleton testing', () {
-      SomeService? intService = di.get<SomeService>();
-      intService.temp = 5;
-      intService = null;
+    test('factory creating is right', () {
+      di.put(InstanceFactoryRegistrar(() => SomeService(1)));
+      expect(di.instanceMap.length, 1);
 
-      expect(di.get<SomeService>().temp, 5);
+      final SomeService firstInstance = di.get();
+      final SomeService secondInstance = di.get();
+
+      expect(firstInstance == secondInstance, false);
     });
-  });
+    test('async factory creating is right', () async {
+      di.put(AsyncFactoryRegistrar(() async => SomeService(1)));
+      expect(di.instanceMap.length, 1);
 
-  group('Future factory testing', () {
-    final TestRegistry di = TestRegistry.instance;
-    test('Future registrar put tests', () {
-      try {
-        di.put(AsyncFactoryRegistrar(() async => SomeService(0)));
-        expect(di.instanceMap.length, 2);
-      } catch (e) {
-        expect(di.instanceMap.length, 2);
-      }
-    });
+      final SomeService firstInstance = await di.get();
+      final SomeService secondInstance = await di.get();
 
-    test('Future registrar getting a new instance', () async {
-      final firstIntFactory = di.get<AsyncFactory<SomeService>>();
-      final secondIntFactory = di.get<AsyncFactory<SomeService>>();
-
-      expect(firstIntFactory == secondIntFactory, true);
-
-      expect((await firstIntFactory()) != (await secondIntFactory()), true);
-
-      final tempService = di.get<SomeService>();
-
-      expect((await firstIntFactory()) != tempService, true);
-    });
-  });
-
-  group('Factory registrars testing', () {
-    final TestRegistry di = TestRegistry.instance;
-
-    test('Factory put', () {
-      di.put(SyncFactoryRegistrar(() => SomeService(0)));
-
-      expect(di.instanceMap.length, 3);
-    });
-
-    test('Factory double put testing', () {
-      try {
-        di.put(SyncFactoryRegistrar(() => SomeService(0)));
-
-        expect(di.instanceMap.length, 3);
-      } catch (e) {
-        expect(di.instanceMap.length, 3);
-      }
-    });
-
-    test('get factory', () {
-      final firstService = di.get<SyncFactory<SomeService>>()();
-      final secondService = di.get<SyncFactory<SomeService>>()();
-
-      expect(firstService != secondService, true);
+      expect(firstInstance == secondInstance, false);
     });
   });
 
   group('drop testing', () {
     final TestRegistry di = TestRegistry.instance;
     test('drop a non existing object', () {
-      final currentObjectLength = di.instanceMap.length;
-      try {
-        di.drop<int>();
-
-        expect(di.instanceMap.length, currentObjectLength);
-      } catch (e) {
-        expect(di.instanceMap.length, currentObjectLength);
-      }
+      expect(() => di.drop<int>(), throwsA(isA<Exception>()));
     });
 
     test('drop an existing singleton', () {
+      di.put(InstanceFactoryRegistrar(() => SomeService(1)));
       final currentObjectLength = di.instanceMap.length;
 
       di.drop<SomeService>();
       expect(di.instanceMap.length, currentObjectLength - 1);
-    });
-
-    test('drop an existing factory', () {
-      final currentObjectLength = di.instanceMap.length;
-
-      di.drop<SyncFactory<SomeService>>();
-      expect(di.instanceMap.length, currentObjectLength - 1);
-    });
-  });
-
-  group('Smart factory', () {
-    final TestRegistry di = TestRegistry.instance;
-    test('put tests', () {
-      final prevLength = di.instanceMap.length;
-
-      di.put(ServiceFactory(
-        (args) => SomeService(args),
-      ));
-
-      expect(di.instanceMap.length, prevLength + 1);
-    });
-
-    test('get tests', () {
-      final serviceFactory = di.get<ServiceFactory>();
-      final serviceInstance = serviceFactory(5);
-
-      expect(serviceInstance.temp, 5);
     });
   });
 }
